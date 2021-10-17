7b81385a1809668612c8ff796d3d48d1e04575df
Rob <robzlegz@gmail.com>
2021-10-17 23:45:54 +0300

    getting top player

--- src/components/MapPiece.tsx
+++ src/components/MapPiece.tsx
@@ -1,43 +1,36 @@
-import React, {useState, useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import { rankingData } from '../../data/ranking';
 
 const MapPiece: React.FC<any> = ({info}) => {
-    const [users, setUsers] = useState<any>([]);
-    const [user, setUser] = useState<any>(null);
+    const [players] = useState<any>([]);
+    const [topPlayer, setTopPlayer] = useState<any>(null);
 
     useEffect(() => {
-        if(info.name){
+        if(info.players){
             rankingData.ranking.forEach((ranking) => {
-                if(!users.includes(ranking) && info.players.includes(ranking.user.username)){
-                    setUsers([...users, ranking]);
-                }
+                info.players.forEach((player: any) => {
+                    if(player === ranking.user.username){
+                        players.push(ranking)
+                    }
+                })
             });
         }
-    }, [info.name, users]);
+    }, [info.players, players]);
 
     useEffect(() => {
-        // console.log(users)
-        // if(!user && users){
-        //     if(users.length === 1){
-        //         setUser(users[0]);
-        //         console.log(user)
-        //     }else{
-        //         let correctOrder = users.sort((a: any, b: any) => {return b.pp - a.pp});
-        //         let winningUser = correctOrder[0];
-        //         setUser(winningUser);
-        //         console.log(user)
-        //     }
-        // }
-        if(users.length > 0){
-            console.log(users)
+        if(players.length > 0){
+            setTopPlayer(players.sort((a: any, b: any) => {return a.pp - b.pp})[0])
         }
-    }, [users, user]);
-    
-    return (
-        <div className="absolute">
-            <img className="" src={info.image} alt="country state borders" />
-        </div>
-    )
+    }, [players, topPlayer]);
+
+    if(topPlayer){
+        return (
+            <div className="absolute">
+                <img className="" src={info.image} alt="country state borders" />
+            </div>
+        )
+    }
+    return null;
 }
 
 export default MapPiece
